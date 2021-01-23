8a74549cc4b85dceb88c852f6dff7f27be97d5ac
Anastasiya Vasileuskaya <ana.vasileuskaya@gmail.com>
2021-01-23 12:48:04 +0100

    Add header small and large inc. text

---
+++ components/_header-small.stories.js
@@ -0,0 +1,9 @@
+import './_header-small.css'
+import html from './_header-small.html'
+
+export default {
+  title: 'Header-small',
+  component: html,
+}
+
+export const base = () => html
--- components/_header.stories.js
+++
@@ -1,9 +0,0 @@
-import './_header.css'
-import html from './_header.html'
-
-export default {
-  title: 'Header',
-  component: html,
-}
-
-export const base = () => html
